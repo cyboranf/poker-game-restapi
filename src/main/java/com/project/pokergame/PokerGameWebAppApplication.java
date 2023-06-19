c69bb95fc7bc57b84657800d7793905c7f42b330
cyboranf <cyboranf@gmail.com>
2023-06-19 21:28:29 +0200

    init

---
+++ src/main/java/com/project/pokergame/PokerGameWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.pokergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerGameWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerGameWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pokergame/PokerGameWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pokergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerGameWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
