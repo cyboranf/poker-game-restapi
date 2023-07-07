1c093f9067b47d416a381ad2afc40ba6353e434f
cyboranf <cyboranf@gmail.com>
2023-07-07 14:40:40 +0200

    method to createPlayerSession - added

--- src/main/java/com/project/pokergame/repository/PlayerSessionRepository.java
+++ src/main/java/com/project/pokergame/repository/PlayerSessionRepository.java
@@ -1,7 +1,12 @@
 package com.project.pokergame.repository;
 
+import com.project.pokergame.model.GameSession;
 import com.project.pokergame.model.PlayerSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlayerSessionRepository extends JpaRepository<PlayerSession, Long> {
+    List<PlayerSession> findByGameSession(GameSession gameSession);
+
 }
--- src/main/java/com/project/pokergame/service/PlayerSessionService.java
+++ src/main/java/com/project/pokergame/service/PlayerSessionService.java
@@ -1,15 +1,88 @@
 package com.project.pokergame.service;
 
+import com.project.pokergame.dto.PlayerSessionDTO;
+import com.project.pokergame.mapper.PlayerSessionMapper;
+import com.project.pokergame.model.GameSession;
+import com.project.pokergame.model.PlayerSession;
+import com.project.pokergame.model.UserAccount;
+import com.project.pokergame.model.UserProfile;
+import com.project.pokergame.repository.GameSessionRepository;
 import com.project.pokergame.repository.PlayerSessionRepository;
+
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+
+import com.project.pokergame.repository.UserProfileRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @Transactional
 public class PlayerSessionService {
     private final PlayerSessionRepository playerSessionRepository;
+    private final UserProfileRepository userProfileRepository;
+    private final GameSessionRepository gameSessionRepository;
+    private final PlayerSessionMapper playerSessionMapper;
 
-    public PlayerSessionService(PlayerSessionRepository playerSessionRepository) {
+    public PlayerSessionService(PlayerSessionRepository playerSessionRepository, UserProfileRepository userProfileRepository, GameSessionRepository gameSessionRepository, PlayerSessionMapper playerSessionMapper) {
         this.playerSessionRepository = playerSessionRepository;
+        this.userProfileRepository = userProfileRepository;
+        this.gameSessionRepository = gameSessionRepository;
+        this.playerSessionMapper = playerSessionMapper;
     }
+
+    /**
+     * Method to create PlayerSession
+     */
+    public PlayerSessionDTO createPlayerSession(Long userId, Long gameId, Double stackSize) {
+        PlayerSession createdPlayerSession = new PlayerSession();
+        UserAccount joiner = userProfileRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("Can not found user with id = " + userId))
+                .getUserAccount();
+
+        GameSession gameSession = gameSessionRepository.findById(gameId)
+                .orElseThrow(() -> new EntityNotFoundException("Can not found game session with id = " + gameId));
+
+
+        createdPlayerSession.setUserAccount(joiner);
+        createdPlayerSession.setGameSession(gameSession);
+        createdPlayerSession.setStackSize(stackSize);
+
+        List<PlayerSession> existingPlayerSessions = playerSessionRepository.findByGameSession(gameSession);
+
+        Set<Integer> takenSeats = existingPlayerSessions.stream()
+                .map(PlayerSession::getSeatNumber)
+                .collect(Collectors.toSet());
+
+        int maxPlayers = gameSession.getGameRoom().getMaxPlayers();
+
+        Integer seatNumber = IntStream.range(1, maxPlayers + 1)
+                .filter(seat -> !takenSeats.contains(seat))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No available seats"));
+
+        createdPlayerSession.setSeatNumber(seatNumber);
+        createdPlayerSession.setIsDealer(false);
+
+        PlayerSession savedPlayerSession = playerSessionRepository.save(createdPlayerSession);
+
+        return playerSessionMapper.toDTO(savedPlayerSession);
+    }
+
+    /**
+     * Method to join Player to Session
+     */
+//    public PlayerSession joinPlayerToSession(Long userId, Long sessionId) {
+//        PlayerSession session = playerSessionRepository.findById(sessionId)
+//                .orElseThrow(() -> new EntityNotFoundException("Can not found session with id = " + sessionId));
+//
+//        UserProfile user = userProfileRepository.findById(userId)
+//                .orElseThrow(() -> new EntityNotFoundException("User not found with id = " + userId));
+//
+//
+//    }
 }
